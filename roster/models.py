fc986b44c767e94020ea525d3a0f40785ae97287
Michael Swartz <mjswartz@gmail.com>
2016-05-26 18:54:34 -0400

    models, team index view

--- roster/models.py
+++ roster/models.py
@@ -1,5 +1,38 @@
-from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+
+@python_2_unicode_compatible
+class Team(models.Model):
+    name = models.CharField(max_length=200)
+    owner = models.ForeignKey(User)
+
+    def __str__(self):
+        return self.name
+
+
+@python_2_unicode_compatible
+class Player(models.Model):
+    name = models.CharField(max_length=200)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+@python_2_unicode_compatible
+class Position(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Abilities(models.Model):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    able = models.BooleanField(default=True)
+
+    class Meta:
+        unique_together = ('position', 'player')
--- roster/views.py
+++ roster/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Team
+
+
+class IndexView(generic.ListView):
+    template_name = 'roster/index.html'
+
+    def get_queryset(self):
+        return Team.objects.order_by('name')
