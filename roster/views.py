bc1a68d07e1eeeb37275d3dcc77870324e723646
Michael Swartz <mjswartz@gmail.com>
2016-05-28 19:51:48 -0400

    switch player create to function. non-valid forms break on player and team

--- roster/views.py
+++ roster/views.py
@@ -7,9 +7,7 @@ from django.views import generic
 from .models import Team, Player, Game, Position, Assignment
 
 
-#############
 ### FORMS ###
-#############
 
 
 class TeamForm(ModelForm):
@@ -17,23 +15,18 @@ class TeamForm(ModelForm):
         model = Team
         fields = ['name']
 
-   # def __init__(self, *args, **kwargs):
-   #     self.request = kwargs.pop('request', None)
-   #     return super(TeamForm, self).__init__(*args, **kwargs)
 
-   # def save(self, *args, **kwargs):
-   #     kwargs['commit'] = False
-   #     team = super(TeamForm, self).save(*args, **kwargs)
-   #     if self.request:
-   #         team.owner = self.request.user
-   #     team.save()
-   #     return team
+class PlayerForm(ModelForm):
+    class Meta:
+        model = Player
+        fields = ['name', 'active']
 
+
+### Teams ###
 
 def index(request):
     teams = Team.objects.order_by('name')
     return render(request, 'roster/index.html', {'teams': teams})
-
 
 def team(request, team_id):
     team = get_object_or_404(Team, pk=team_id)
@@ -49,29 +42,21 @@ def team_create(request):
 
     return render(request, 'roster/team_form.html', {'form': form})
 
+### Players ###
 
-# class TeamCreate(generic.CreateView):
-#     model = Team
-#     template_name = 'roster/team_form.html'
-#     fields = ['name', 'owner']
+def player_create(request, team_id):
+    team = get_object_or_404(Team, pk=team_id)
+    form = PlayerForm(request.POST or None)
+    player = form.save(commit=False)
+    player.team = team
 
+    if form.is_valid():
+        player.save()
+        return HttpResponseRedirect(reverse('roster:team', args=(team.id,)))
 
-class PlayerCreate(generic.CreateView):
-    model = Player
-    template_name = 'roster/player_form.html'
-    fields = ['name', 'active']
+    return render(request, 'roster/player_form.html', {'form': form})
 
-    def dispatch(self, *args, **kwargs):
-        self.team = get_object_or_404(Team, pk=kwargs['team_id'])
-        return super(PlayerCreate, self).dispatch(*args, **kwargs)
-
-    def form_valid(self, form):
-        self.object = form.save(commit=False)
-        self.object.team = self.team
-        self.object.save()
-        return HttpResponseRedirect(self.get_success_url())
-
-
+### Games ###
 
 class GameCreate(generic.CreateView):
     model = Game
