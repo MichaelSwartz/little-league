1743d0f939652cece6b1f7463350fbb0caa1481a
Michael Swartz <mjswartz@gmail.com>
2016-05-28 12:13:01 -0400

    change remaining templates to use base. make player form automatically set team

--- roster/urls.py
+++ roster/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^(?P<pk>[0-9]+)/$', views.TeamView.as_view(), name='team'),
     url(r'^create/$', views.TeamCreate.as_view(), name='team_form'),
-    url(r'^add_player/$', views.PlayerCreate.as_view(), name='player_form'),
+    url(r'^(?P<team_id>[0-9]+)/add_player/$', views.PlayerCreate.as_view(), name='player_form'),
 ]
--- roster/views.py
+++ roster/views.py
@@ -1,5 +1,5 @@
 from django.core.urlresolvers import reverse_lazy, reverse
-
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.views import generic
 
@@ -30,7 +30,17 @@ class TeamCreate(generic.CreateView):
 class PlayerCreate(generic.CreateView):
     model = Player
     template_name = 'roster/player_form.html'
-    fields = ['name', 'team']
+    fields = ['name', 'active']
+
+    def dispatch(self, *args, **kwargs):
+        self.team = get_object_or_404(Team, pk=kwargs['team_id'])
+        return super(PlayerCreate, self).dispatch(*args, **kwargs)
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.team = self.team
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
 
     # def get_success_url(self):
     #     return reverse_lazy('roster:team', args=[str(self.team.id)])
